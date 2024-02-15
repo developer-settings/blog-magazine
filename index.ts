6cb878e2751f247ea705ed45e52479f06c00af60
Developer Settings <developersettings007@gmail.com>
2024-02-15 15:31:04 -0500

    install express js

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import home from './routes/home';
+
+const app = express();
+app.use(express.json());
+
+app.use('/', home);
+
+const port = Bun.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
